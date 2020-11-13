90a09f2a25669d45cde5c0b14baf2013b0983590
ajay-creator <ajayveeraveni119@gmail.com>
2020-11-14 00:50:25 +0530

    static

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = [Path(BASE_DIR / "staticfiles"),]
+STATIC_ROOT = Path(BASE_DIR / "staticfiles")
 
 STATIC_URL = '/static/'
 STATIC_DIR = [Path(BASE_DIR / 'static'),]
