004bc9cbef1c67f2f897594e4b7d672226e37caa
ajay-creator <ajayveeraveni119@gmail.com>
2020-11-14 00:21:01 +0530

    third commit

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = [Path(BASE_DIR / "staticfiles"),]
+
 STATIC_URL = '/static/'
 STATIC_DIR = [Path(BASE_DIR / 'static'),]
